1aa0aa9aeda4e99df00bf226f9a0e2b8d4b934b7
rlarjsdn3 <rlarjsdn3@naver.com>
2025-06-25 23:52:33 +0900

    commit

--- MobileBanking/Core/TabBar/BankingTabBar.swift
+++ MobileBanking/Core/TabBar/BankingTabBar.swift
@@ -8,5 +8,27 @@
 import UIKit
 
 final class BankingTabBar: UITabBar {
-    
+
+    var shapeLayer: CAShapeLayer?
+
+    override func draw(_ rect: CGRect) {
+        let capsule = UIBezierPath(
+            roundedRect: rect,
+            cornerRadius: rect.height / 2
+        )
+
+        if let oldShapeLayer = shapeLayer {
+            oldShapeLayer.removeFromSuperlayer()
+        }
+        shapeLayer = CAShapeLayer()
+        shapeLayer?.path = capsule.cgPath
+        shapeLayer?.fillColor = UIColor.bankingBlack.resolvedColor(with: traitCollection).cgColor
+        shapeLayer?.frame = rect
+        self.layer.insertSublayer(shapeLayer!, at: 0)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setNeedsDisplay(bounds)
+    }
 }
---
+++ MobileBanking/Core/TabBar/BankingTabBarController.swift
@@ -0,0 +1,56 @@
+//
+//  BankingTabBarController.swift
+//  MobileBanking
+//
+//  Created by 김건우 on 6/23/25.
+//
+
+import UIKit
+
+final class BankingTabBarController: UITabBarController {
+
+    let tabHeight: CGFloat = 95.0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTabBarAppearance()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        self.tabBar.frame.size.height = tabHeight
+        self.tabBar.frame.origin.y = view.frame.height - tabHeight
+        self.tabBar.setNeedsDisplay()
+    }
+
+    private func setupTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+
+        let attributesNormal: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 12, weight: .semibold),
+            .foregroundColor: UIColor.gray
+        ]
+
+        let attributesSelected: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 12, weight: .semibold),
+            .foregroundColor: UIColor.white
+        ]
+
+        let itemAppearance = UITabBarItemAppearance()
+        itemAppearance.normal.iconColor = .gray
+        itemAppearance.normal.titleTextAttributes = attributesNormal
+        itemAppearance.normal.titlePositionAdjustment = .init(horizontal: 0, vertical: 5)
+        itemAppearance.selected.iconColor = .white
+        itemAppearance.selected.titleTextAttributes = attributesSelected
+        itemAppearance.selected.titlePositionAdjustment = .init(horizontal: 0, vertical: 5)
+
+        appearance.stackedLayoutAppearance = itemAppearance
+        appearance.compactInlineLayoutAppearance = itemAppearance
+        appearance.inlineLayoutAppearance = itemAppearance
+
+        self.tabBar.standardAppearance = appearance
+        self.tabBar.scrollEdgeAppearance = appearance
+    }
+}
